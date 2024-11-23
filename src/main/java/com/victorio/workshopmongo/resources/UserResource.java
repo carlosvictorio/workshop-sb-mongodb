2a008850cda36d5cefbb9dc44b74ca4bc5bab843
victorio nogueira <victorionogueira@gmail.com>
2024-11-23 19:33:50 -0300

    UserById, Exception Handler

--- src/main/java/com/victorio/workshopmongo/resources/UserResource.java
+++ src/main/java/com/victorio/workshopmongo/resources/UserResource.java
@@ -1,11 +1,13 @@
 package com.victorio.workshopmongo.resources;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,5 +28,13 @@ public class UserResource {
 		List<UserDTO> userDto = users.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(userDto);
 	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+		User user = service.findById(id);
+		UserDTO userDto = new UserDTO(user);
+		return ResponseEntity.ok().body(userDto);
+	}
+	
 
 }
